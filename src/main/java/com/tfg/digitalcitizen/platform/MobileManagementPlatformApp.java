2f381b446044defb49b6a4d71b71de31797b0886
Cfval <francesvaldesc@gmail.com>
2025-11-12 10:13:45 +0100

    Feat: pom modified, PostgreSQL db integrated

--- src/main/java/com/tfg/digitalcitizen/platform/MobileManagementPlatformApp.java
+++ src/main/java/com/tfg/digitalcitizen/platform/MobileManagementPlatformApp.java
@@ -1,4 +1,4 @@
-package com.tfg.digitalcitizen.platform.app;
+package com.tfg.digitalcitizen.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
